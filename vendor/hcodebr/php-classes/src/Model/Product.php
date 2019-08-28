ff2476af9f53661c2c243c7fce33e123c9d6cff0
Bruno Litzenberger <litzenberger.bruno@gmail.com>
2019-08-28 15:06:02 -0300

    Site - Produtos

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -5,8 +5,6 @@ namespace Hcode\Model;
 use \Hcode\DB\Sql;
 use \Hcode\Model;
 use \Hcode\Mailer;
-use \Hcode\Model\User;
-use \Hcode\Model\Product;
 
 class Product extends Model {
 
@@ -18,6 +16,22 @@ class Product extends Model {
 		return $sql->select("SELECT * FROM tb_products ORDER BY desproduct");
 
 	}
+
+	public static function checkList($list){
+
+		foreach ($list as &$row) {
+			
+			$p = new Product();
+			$p->setData($row);
+			$row = $p->getValues();
+
+		}
+
+		return $list;
+
+	}
+
+	
 
 	public function save(){
 		$sql = new Sql();
